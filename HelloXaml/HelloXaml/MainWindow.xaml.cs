9db6263f3ad7f73de4653f542aa89e7979e01cb4
Tibor Tóth <toth.tibor@autsoft.hu>
2023-09-08 14:54:27 +0200

    add person

--- HelloXaml/HelloXaml/MainWindow.xaml.cs
+++ HelloXaml/HelloXaml/MainWindow.xaml.cs
@@ -3,13 +3,14 @@ using HelloXaml.Models;
 using Microsoft.UI.Xaml;
 
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace HelloXaml;
 
 public sealed partial class MainWindow : Window
 {
     public Person NewPerson { get; set; }
-    public List<Person> People { get; set; }
+    public ObservableCollection<Person> People { get; set; }
 
     public MainWindow()
     {
@@ -21,7 +22,7 @@ public sealed partial class MainWindow : Window
             Age = 8
         };
 
-        People = new List<Person>()
+        People = new ObservableCollection<Person>()
         {
           new Person() { Name = "Peter Griffin", Age = 40 },
           new Person() { Name = "Homer Simpson", Age = 42 },
@@ -32,6 +33,7 @@ public sealed partial class MainWindow : Window
 
     private void AddButton_Click(object sender, RoutedEventArgs e)
     {
+        People.Add(NewPerson);
     }
 
     private void DecreaseButton_Click(object sender, RoutedEventArgs e)
